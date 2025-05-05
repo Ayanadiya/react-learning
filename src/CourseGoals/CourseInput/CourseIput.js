57986466ed6013fbaf49ef9c7f69348eace028ea
ayanadiya <ayanadiya17@gmail.com>
2025-05-05 11:47:26 +0530

    Dynamic stylling using class component css

--- src/CourseGoals/CourseInput/CourseIput.js
+++ src/CourseGoals/CourseInput/CourseIput.js
@@ -29,9 +29,9 @@ const CourseInput = (props) => {
 
   return (
     <form onSubmit={formSubmitHandler}>
-      <div className="form-control">
-        <label htmlFor="goalInput" style={{color:!isValid ?"red":"black"}}>Course Goal</label>
-        <input id="goalInput" type="text" onChange={goalInputChangeHandler} style={{background:!isValid?"lightpink":"transparent", borderColor:!isValid ? "red":"black"}} />
+      <div className={`form-control ${isValid? "": "invalid"}`}>
+        <label htmlFor="goalInput" >Course Goal</label>
+        <input id="goalInput" type="text" onChange={goalInputChangeHandler}  />
       </div>
       <Button type="submit">Add Goal</Button>
     </form>
